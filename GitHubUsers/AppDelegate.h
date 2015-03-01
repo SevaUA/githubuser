fdc3a7a1e0f7f5459ea626dbefab6983513fb9cf
Sergey Pulyaev <serj1903@gmail.com>
2015-03-01 12:56:05 +0200

    Initial Commit

---
+++ GitHubUsers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubUsers
+//
+//  Created by Sergey Pulyaev on 01.03.15.
+//  Copyright (c) 2015 Sergey Pulyaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
